67a2faffd8c500acf360e13be1c3df2cbac7a372
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-08-29 16:11:22 -0400

    extract & filter weather data by desired info

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable unicorn/prefer-top-level-await */
+/* eslint-disable n/no-unsupported-features/node-builtins */
 // #region required-template
 import './reset.css';
 import './style.css';
+// [ ]TODO: fetch API
+// [ ]TODO: process json
+// [ ]TODO: input form (location)
+// [ ]TODO: error handling
+
+// [ ]TODO: "loading time" + test on DevTools for low end devices
 
 // eslint-disable-next-line no-unused-vars
 const testElement = document.createElement('div');
 // #endregion
 
-
 // DOM elements
 const domInputs = document.querySelectorAll('input');
 /** @type {Array.<HTMLInputElement>} */
-const [
-  passwordOriginal = document.querySelector('input#password'),
-  passwordToCompare = document.querySelector('input#confirm-password'),
-  email = document.querySelector('input#email'),
-  country = document.querySelector('#country'),
-  zip = document.querySelector('#zip'),
-  form = document.querySelector('form'),
-] = [];
+const [passwordOriginal = document.querySelector('input#password')] = [];
+
+// API functions
+const apiString =
+  'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York?unitGroup=us&key=DZE4DLL8Y94HFL5G8DRAWPKgg&contentType=json';
+
+async function getWeather() {
+  const response = await fetch(apiString, { mode: 'cors' });
+  const responseJSON = await response.json();
+  return responseJSON;
+}
+
+function handleError(unsafeFunction) {
+  return function () {
+    return unsafeFunction().catch(function (error) {
+      console.log(error);
+      // throw error;
+    });
+  };
+}
+
+const getWeatherSafe = handleError(getWeather);
+const weatherObject = await getWeatherSafe();
+console.log('weatherObj:', weatherObject);
+
+// function processWeatherObject(weatherObject_) {}
+
+// const destructureWeatherObject = ({ address, description }) => ({ address, description });
+
+function destructureWeatherObject({ address, description, currentConditions, days }) {
+  return { address, description, currentConditions, days };
+}
+const weatherObjectFiltered = destructureWeatherObject(weatherObject);
+const desiredWeatherInfo = ['temp', 'feelslike', 'humidity'];
+const wantedInfo = Object.entries(weatherObjectFiltered.currentConditions).filter(([key, value]) =>
+  desiredWeatherInfo.includes(key)
+);
+
+console.log('wantedInfo:', wantedInfo)
